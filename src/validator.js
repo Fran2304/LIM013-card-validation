c754a53dbc86283d3eaa5bf962275f4ab254a790
Fran2304 <lilatiravanttiarteaga@gmail.com>
2020-07-21 13:17:41 -0500

    funciones is valid y masquifi en index

--- src/validator.js
+++ src/validator.js
@@ -1,4 +1,4 @@
-/*const validator = {name:"juan", mail:"juan@gmail.com", card: "123456789", csv: "1234", expireDate: "120822021"
+/*ckconst validator = {name:"juan", mail:"juan@gmail.com", card: "123456789", csv: "1234", expireDate: "120822021"
   // ...
 };
 export default validator;*/
