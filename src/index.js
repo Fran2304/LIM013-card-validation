92d5ca8122735210633c528709d417c88cd4f881
Fran2304 <lilatiravanttiarteaga@gmail.com>
2020-07-21 09:54:35 -0500

    is valid y maskify en index.js listo

--- src/index.js
+++ src/index.js
@@ -1,5 +1,50 @@
-import validator from './validator.js';
-alert("hi");
-console.log(validator);
+/*import validator from './validator.js';
+console.log(validator);*/
 
 
+const buttonPay = document.getElementById("buttonPay");
+buttonPay.addEventListener('click',() => {
+  let cardNumber = document.getElementById("cardNumber").value;
+  if (cardNumber.length === 0) {
+    document.getElementById("incorrectCard").innerHTML= "Ingrese un número de tarjeta";
+  }
+
+  const validator = { 
+    creditCardNumber:cardNumber,
+    isValid :function() {
+      let converToNumber=validator.creditCardNumber;
+      let arrayCard= Array.from(converToNumber);
+      let reverseCard= arrayCard.reverse();
+      let suma=0;
+      for (let i=0;i<=reverseCard.length - 1;i++){
+        if(i%2 !== 0){
+          suma+= parseInt(reverseCard[i]);
+        }else if((parseInt(reverseCard[i]) * 2) <=9){
+          suma+= parseInt(reverseCard[i]*2);
+        }else{
+          suma+=(parseInt((parseInt(reverseCard[i]*2)).toString().charAt(0))+parseInt((parseInt(reverseCard[i]*2)).toString().charAt(1)));
+        }
+      };
+      return suma%10===0;
+    },
+    
+    maskify: function () {
+      let stringCard=validator.creditCardNumber;
+      let newString="";
+       //remplaza x #
+      for (let i=0;i<stringCard.length;i++){
+        if(i>=(stringCard.length-4)){
+          newString += stringCard.charAt(i);
+        } else {
+          newString += "#";
+        }
+      }
+      return newString;
+    }
+
+  };
+  
+  console.log (validator.isValid());
+  console.log (validator.maskify());
+  document.getElementById("encripted").innerHTML= (validator.maskify());
+});
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,6 @@
-const validator = {
+/*const validator = {name:"juan", mail:"juan@gmail.com", card: "123456789", csv: "1234", expireDate: "120822021"
   // ...
 };
+export default validator;*/
 
-export default validator;
+
