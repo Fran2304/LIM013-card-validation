5449dc15c4eae642b203514a583480e371574a30
Fran2304 <lilatiravanttiarteaga@gmail.com>
2020-07-24 22:05:56 -0500

    funciones en validatos + pruebas+ css basico

--- src/index.js
+++ src/index.js
@@ -1,33 +1,39 @@
 import validator from './validator.js';
 //console.log(validator);
 
-const buttonPay = document.getElementById("buttonPay");
-buttonPay.addEventListener('click',() => {
-  let cardNumber = document.getElementById("cardNumber").value;
-  if (cardNumber.length === 0) {
-    document.getElementById("incorrectCard").innerHTML= "Ingrese un número de tarjeta";
-  }
-  let cardValid=validator.isValid(cardNumber);
-  let cardMask=validator.maskify(cardNumber);
-  
-  console.log(cardValid);
-  console.log(cardMask);
-   
-// pasar a la siguiente view si la tarjeta es valida y no es vacía. Mostrar error sino :C
-
+let catchCard=document.getElementById("cardNumber");
+let incorrectCard=document.getElementById("incorrectCard");
+incorrectCard.style.color="#F25CA2";
+let encriptedCard=document.getElementById("encripted");
 let thirdView=document.getElementById("thirdView");
 let secondView=document.getElementById("secondView");
 
-if (cardNumber.length !== 0){
+
+const buttonPay = document.getElementById("buttonPay");
+buttonPay.addEventListener("click",() => {
+  let cardNumber = catchCard.value;
+//llama a las funciones de validator
+  let cardValid=validator.isValid(cardNumber);
+  let cardMask=validator.maskify(cardNumber);  
+// Si la tarjeta es diferente a vacío y válida, pasar a la siguiente vista. De lo contrario error. :C
+if (cardNumber.length !== 0 && cardNumber !== "0"){
   if(cardValid===true){
-    thirdView.style.display="block";
+    thirdView.style.display="flex";
     secondView.style.display ="none";
   }else {
-    document.getElementById("incorrectCard").innerHTML= "El número de tarjeta es inválido";
+    incorrectCard.textContent= "*El número de tarjeta es inválido";
   }
 } else{
-  document.getElementById("incorrectCard").innerHTML= "Ingrese un número de tarjeta";
+  incorrectCard.textContent= "*Ingrese un número de tarjeta";
 }
+//Coloca la tarjeta encriptada
+encriptedCard.innerHTML= (cardMask);
+//Coloca los datos del cliente  en la tercera vista
+let name=document.getElementById("name").value;
+let email=document.getElementById("email").value;
+let nameCatch=document.getElementById("nameCatch");
+let emailCatch=document.getElementById("emailCatch");
+nameCatch.innerHTML=(name);
+emailCatch.innerHTML=(email);
 
-document.getElementById("encripted").innerHTML= (cardMask);
 });
--- src/validator.js
+++ src/validator.js
@@ -1,24 +1,23 @@
 const validator = {
-  // ...
   isValid :function(creditCardNumber) {
-    //let converToNumber=validator.creditCardNumber;
+    
     let arrayCard= Array.from(creditCardNumber);
     let reverseCard= arrayCard.reverse();
     let suma=0;
     for (let i=0;i<=reverseCard.length - 1;i++){
-      if(i%2 !== 0){
+      
+      if(i%2 === 0){
         suma+= parseInt(reverseCard[i]);
       }else if((parseInt(reverseCard[i]) * 2) <=9){
         suma+= parseInt(reverseCard[i]*2);
       }else{
         suma+=(parseInt((parseInt(reverseCard[i]*2)).toString().charAt(0))+parseInt((parseInt(reverseCard[i]*2)).toString().charAt(1)));
       }
-    };
+    }
     return suma%10===0;
   },
 
   maskify: function (creditCardNumber) {
-    //let stringCard=validator.creditCardNumber;
     let newString="";
      //remplaza x #
     for (let i=0;i<creditCardNumber.length;i++){
